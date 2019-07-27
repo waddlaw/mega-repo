07146c197916ff3a49f7cb989dc96aba76b783cd
waddlaw <ingronze@gmail.com>
2019-07-27 15:06:57 +0900

    first commit

---
+++ mega-repo1/src/MegaExample1.hs
@@ -0,0 +1,4 @@
+module MegaExample1 where
+
+megaExample1 :: String
+megaExample1 = "mega repo example1"
---
+++ mega-repo2/src/MegaExample2.hs
@@ -0,0 +1,5 @@
+module MegaExample2 where
+
+megaExample2 :: String
+megaExample2 = "mega repo example2"
+
